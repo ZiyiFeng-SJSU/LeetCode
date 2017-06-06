f73565aa6ee80460d52a08d12bff4edd4835481f
ZiyiFeng <ziyi.feng@sjsu.edu>
2017-06-05 19:25:51 -0700

    461 Done

--- HammingDistance_461.cpp
+++ HammingDistance_461.cpp
@@ -1,6 +1,31 @@
-class Solution {
+#include <iostream>
+#include <bitset>
+using namespace std;
+
+class Solution
+{
 public:
-    int hammingDistance(int x, int y) {
-        
+  int hammingDistance(int x, int y)
+  {
+    bitset<32> X(x);
+    bitset<32> Y(y);
+
+    int counter = 0;
+    // int maxSize = ((X.size() > Y.size()) ? X.size() : Y.size());
+    for (int i = 0; i < 32; i++)
+    {
+      if (X[i] != Y[i])
+      {
+        counter++;
+        X >> 2;
+        Y >> 2;
+      }
     }
+    return counter;
+  }
 };
+int main()
+{
+  Solution S;
+  cout << S.hammingDistance(1, 4);
+}
